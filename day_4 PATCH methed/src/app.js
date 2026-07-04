b0ddaf16742d9f4e1b82d5e9ee101d55f6ce9b63
Aloukik Patel <41272007+Aloukik122@users.noreply.github.com>
2026-07-04 23:58:43 +0530

    patch is done

--- day_4 PATCH methed/src/app.js
+++ day_4 PATCH methed/src/app.js
@@ -29,4 +29,29 @@ app.delete('/notes/:index', (req, res) => {
   });
 });
 
+//------------------------------------------------------------------------------------------------
+
+/* kisi be notes ka descreption change ya update krna ho to PATCH ka use krenge */
+
+app.patch('/notes/:index', (req, res) => {
+  const index = req.params.index; //-------kon se note ka descreption update krna h uske lye index pta kr rhe h
+
+  const descreption = req.body.descreption; //---------- req.body.descreption -- se jo hm "naya descreption" send krenge vo ayega
+
+  notes[index].descreption = descreption; //---------- notes [ ka jo index eg. 1,2,13] uske ".descreption" ko new " = descreption" se change kr do ya uske brabr kr do
+
+  /* challange --->
+
+        title option bhi add krna h kise krenge
+
+*/
+
+  res.status(200).json({
+    message: 'note update successfully',
+  });
+});
+
+//------------------------------------------------------------------------------------------------------
+/* task " add PATCH option for (title) so usko bhi (descreption) ke jise update kr ske" */
+
 module.exports = app;
